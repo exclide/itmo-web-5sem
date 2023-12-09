c9626b95d81ebc78afa9efb6f8a0d634baba22c8
w <exclide1@gmail.com>
2023-12-09 18:17:29 +0300

    add localStorage

--- js/modal.js
+++ js/modal.js
@@ -1,18 +1,32 @@
 $(document).ready(function(){
+    // При загрузке страницы проверяем, сохранены ли данные о пользователе в localStorage
+    const storedUsername = localStorage.getItem('loggedInUsername');
+
+    if (storedUsername) {
+        // Если данные о пользователе сохранены, показываем блок с информацией о пользователе
+        $('#authContainer').hide();
+        $('#userInfo').show();
+        $('#loggedInUsername').text(storedUsername);
+    }
+
     $('#loginForm').submit(function(event) {
         event.preventDefault();
 
         const username = $('#username').val();
         const password = $('#password').val();
-        // Пароль не проверяется, т.к. в jsonplaceholder ручке его нет
 
         $.ajax({
             url: `https://jsonplaceholder.typicode.com/users?username=${username}`,
             method: 'GET',
             success: function(users) {
                 if (users.length > 0) {
-                    // В случае, если пользователь существует, моделируем успешный логин
+                    // В случае успешной аутентификации
                     const user = users[0];
+
+                    // Сохраняем информацию о пользователе в localStorage
+                    localStorage.setItem('loggedInUsername', user.name);
+
+                    // Показываем блок с информацией о пользователе
                     $('#authContainer').hide();
                     $('#userInfo').show();
                     $('#loggedInUsername').text(user.name);
